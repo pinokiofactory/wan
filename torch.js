25f96f8b6633a51181491833ca9f52ff7c837c53
ai_anchorite <ai_anchorite@proton.me>
2025-09-28 05:23:15 +1300

    Update torch.js

--- torch.js
+++ torch.js
@@ -55,8 +55,7 @@ module.exports = {
         "message": [
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
           "uv pip install git+https://github.com/thu-ml/SageAttention.git",
-          "uv pip install https://github.com/kingbri1/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu128torch2.7.0cxx11abiFALSE-cp310-cp310-linux_x86_64.whl",
-          
+          "uv pip install https://github.com/kingbri1/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu128torch2.7.0cxx11abiFALSE-cp310-cp310-linux_x86_64.whl"          
         ]
       }
     },
