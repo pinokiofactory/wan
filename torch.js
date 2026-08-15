dce8029cd470cfa0567f3de33608e485ddc40a5d
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2026-08-15 17:40:06 +0200

    remove bitsandbytes for windows amd
    official pypi wheel doesn't support windows amd despite listed as supported on the suport tabe on the github repo.

--- torch.js
+++ torch.js
@@ -88,8 +88,7 @@ module.exports = {
         "message": [
           "uv pip install https://github.com/scottt/rocm-TheRock/releases/download/v6.5.0rc-pytorch-gfx110x/torch-2.7.0a0+rocm_git3f903c3-cp311-cp311-win_amd64.whl",
           "uv pip install https://github.com/scottt/rocm-TheRock/releases/download/v6.5.0rc-pytorch-gfx110x/torchaudio-2.7.0a0+52638ef-cp311-cp311-win_amd64.whl",
-          "uv pip install https://github.com/scottt/rocm-TheRock/releases/download/v6.5.0rc-pytorch-gfx110x/torchvision-0.22.0+9eb57cd-cp311-cp311-win_amd64.whl",
-          "uv pip install bitsandbytes==0.49.2 --no-deps"
+          "uv pip install https://github.com/scottt/rocm-TheRock/releases/download/v6.5.0rc-pytorch-gfx110x/torchvision-0.22.0+9eb57cd-cp311-cp311-win_amd64.whl"
         ]
       },
       "next": null
